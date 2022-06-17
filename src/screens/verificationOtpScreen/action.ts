5e9a55c6aa8f80ea425276202ac7b789ad03bcb8
Harsh Vardhan Kushwaha <harsh.vardhan@appinventiv.com>
2022-06-17 10:51:44 +0530

    fourthcommit

---
+++ src/screens/verificationOtpScreen/action.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+export default function OtpAction(str: any) {
+  console.log('firstactionkadata', str);
+  return (getState: any) => {
+    const {
+      signUpReducer: {userId, countryCode, phoneNo},
+    } = getState();
+    axios({
+      method: 'post',
+      url: 'https://fivestardevapi.appskeeper.in/api/v1/user/verify-only-otp',
+      data: {
+        userId: userId,
+        otp: str,
+        countryCode: countryCode,
+        phoneNo: phoneNo,
+      },
+    })
+      .then(resp => console.log('responseOTP', resp))
+      .catch(error => console.log('error', error));
+  };
+}
